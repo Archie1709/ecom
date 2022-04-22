8982ce12680207bc365095e287ed86fd693ed1c3
archie <learning.archie@gmail.com>
2022-04-23 00:23:08 +0530

    ecom-v-1.0

---
+++ ecom/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='#' className='brand-logo'>EcommLogo</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
